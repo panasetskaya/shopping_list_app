72499ce4706098021c94bb1927e6635c999496fa
Maria Panasetskaia <mariapolikutina@gmail.com>
2022-03-21 14:16:38 +0800

    data layer: repository implementation

--- app/src/main/java/com/example/myshoppinglist/domain/ShopItem.kt
+++ app/src/main/java/com/example/myshoppinglist/domain/ShopItem.kt
@@ -1,8 +1,12 @@
 package com.example.myshoppinglist.domain
 
 data class ShopItem(
-    val id: Int,
     val name: String,
     val quantity: Int,
-    val isActive: Boolean
-)
+    var isActive: Boolean = true,
+    var id: Int = UNDEFINED_ID
+) {
+    companion object {
+        const val UNDEFINED_ID = -1
+    }
+}
